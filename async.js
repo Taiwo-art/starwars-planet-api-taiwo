7d6754190c03c705c9697559ea3ba72e91050ac9
Taiwo Lambo <lmb.taiwo@gmail.com>
2022-09-01 13:28:04 +0100

    classwork

---
+++ async.js
@@ -0,0 +1,10 @@
+///extract data///
+const getPlanetApi = async() => {
+    let planetApi = await fetch("https://swapi.dev/api/planets");
+    let planetData = await planetApi.json();
+    let finalData = planetData.results
+        let result = finalData.map(planet =>`<li> name: ${planet.name} ---- rotation period: ${planet.rotation_period} --- population: ${planet.population}  ---${planet.residents}</li>`)
+        return document.body.innerHTML = result.join('');
+    
+}
+getPlanetApi()
